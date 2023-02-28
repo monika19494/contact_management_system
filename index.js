5e17be073528fde55ffafa4ce8486c8dc336f70d
monika <monikaraghuwanshi1904@gmail.com>
2023-02-28 23:09:59 +0530

    added api

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-app.use("/v1/contact", require("./routes/v1/contact"));
-const router = require("./routes/v1/contact");
+app.use("/api/contact", require("./routes/api/contact"));
+const router = require("./routes/api/contact");
 app.listen(5000, () => console.log("server running at 5000"));
